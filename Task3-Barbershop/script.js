b30dc77a57c62c30e0c4997174bd4a72589865c8
Olwethu7 <132436323+Olwethu7@users.noreply.github.com>
2026-03-23 17:57:55 +0200

    Create script.js

---
+++ Task3-Barbershop/script.js
@@ -0,0 +1,11 @@
+// Smooth Scroll
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+// Console log to check JS
+console.log("New Legacy Barbershop website loaded successfully!");
